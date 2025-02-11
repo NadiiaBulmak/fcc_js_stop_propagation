f14104249e3842e0873f0f5f8f99dae3073c4cba
Nadiia Bulmak <nadgaval@gmail.com>
2025-02-11 14:41:28 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+document.querySelector('.modal-overlay').addEventListener('click', () => {
+  document.querySelector('.modal-overlay').style.display = 'none';
+  // console.log('Modal closed by clicking outside');
+});
+
+document.querySelector('.modal-content').addEventListener('click', (event) => {
+  event.stopPropagation();
+});
+
+document.querySelector('.close-btn').addEventListener('click', (event) => {
+  event.stopPropagation();
+  document.querySelector('.modal-overlay').style.display = 'none';
+  // console.log('Modal closed with button');
+});
+
+document.querySelector('.submit-btn').addEventListener('click', (event) => {
+  event.preventDefault();
+  event.stopPropagation();
+  // console.log('Form submitted!');
+});
